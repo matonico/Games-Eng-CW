04cbb790c2845cbe205352a69cacc5a545187028
ProperUmgak <meduseff@gmail.com>
2023-04-21 14:44:43 +0100

    Revert "Merge branch 'huba' into develop"
    This reverts commit 82c49b340177b8e5074204a43b4868ab433974cf, reversing
    changes made to 36b0307f54f5e1d94db6112d38ea942b713e7f21.

--- CWsrc/scenes/scene_menu.cpp
+++ CWsrc/scenes/scene_menu.cpp
@@ -46,50 +46,5 @@ void MenuScene::Update(const double& dt) {
     Engine::ChangeScene(&level1);
   }
 
-    if (exitButton->getText()->getGlobalBounds().contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition(Engine::GetWindow())))) {
-        exitButton->getText()->setFillColor(sf::Color::Red);
-        if (Mouse::isButtonPressed(Mouse::Left())) {
-            Engine::GetWindow().close();
-            return;
-        }
-    }
-    else {
-        exitButton->getText()->setFillColor(sf::Color::White);
-    }
-
-  //play button
-    auto playEnt = ents.find("Play")[0]->GetCompatibleComponent<TextComponent>();
-    shared_ptr<TextComponent>playButton = playEnt.front();
-   
-  if (playButton->getText()->getGlobalBounds().contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition(Engine::GetWindow())))) {
-      playButton->getText()->setFillColor(sf::Color::Red);
-      if (Mouse::isButtonPressed(Mouse::Left())) {
-          Engine::ChangeScene(&level1);
-          return;
-      }
-  }
-  else {
-      playButton->getText()->setFillColor(sf::Color::White);
-  }
-  
-  //settings button
-    auto settingsEnt = ents.find("Settings")[0]->GetCompatibleComponent<TextComponent>();
-    shared_ptr<TextComponent>settingsButton = settingsEnt.front();
-
-  if (settingsButton->getText()->getGlobalBounds().contains(static_cast<sf::Vector2f>(sf::Mouse::getPosition(Engine::GetWindow())))) {
-      settingsButton->getText()->setFillColor(sf::Color::Red);
-      if (Mouse::isButtonPressed(Mouse::Left())) {
-          Engine::ChangeScene((Scene*)&settings);
-          return;
-      }
-
-  }
-  else {
-      settingsButton->getText()->setFillColor(sf::Color::White);
-  }
-  
-   
-  
-  
   Scene::Update(dt);
 }
